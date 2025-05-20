26785ef0a5ba73c93574638917332c1ffb0b23a6
Kiel1mcc <KIEL.MCCLEARY@GMAIL.COM>
2025-05-20 17:31:43 -0400

    Add files via upload

---
+++ drivepath-netlify-final/src/components/ui/label.jsx
@@ -0,0 +1,3 @@
+export function Label({ children }) {
+  return <label className="block font-medium text-gray-700">{children}</label>;
+}
---
+++ drivepath-netlify-final/src/components/ui/textarea.jsx
@@ -0,0 +1,3 @@
+export function Textarea(props) {
+  return <textarea className="w-full p-2 border border-gray-300 rounded-md" {...props} />;
+}
